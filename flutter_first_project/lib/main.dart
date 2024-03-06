46eeaa635adc2e1d97c9ff7aec2935cc1ea41d2e
Shaheer Ahmed <shaheer.ahmed0404@gmail.com>
2024-03-06 18:02:44 +0500

    Creating App bar in Scaffold in main File

---
+++ flutter_first_project/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(const myApp());
+}
+
+class myApp extends StatelessWidget {
+  const myApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("This is My First Application"),
+        ),
+      ),
+    );
+  }
+}
